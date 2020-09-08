d23c2eb74bf4939ef7797512b5442cab5e88eaf8
Sergio Neto <sergio.s.neto@outlook.com>
2020-09-08 16:13:27 -0300

    divisão de tarefas

--- app.py
+++ app.py
@@ -3,12 +3,11 @@ import jwt
 from flask import Flask, request, jsonify
 from flask_json import FlaskJSON, JsonError, json_response, as_json
 from flask_sqlalchemy import SQLAlchemy
-# from flask_request_params import bind_request_params
+
 
 app = Flask(__name__)
 app.config.from_object('config.DevelopmentConfig')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# app.before_request(bind_request_params)
 FlaskJSON(app)
 db = SQLAlchemy(app)
 
@@ -22,6 +21,7 @@ def authorization(token):
     except jwt.InvalidTokenError:
         return False
 
+# Rota de Cadastro
 @app.route('/cadastro', methods=['POST'])
 def cadastrar():
     from models import User
@@ -40,6 +40,7 @@ def cadastrar():
     except Exception as e:
         return jsonify(e)
 
+# Rota de login
 @app.route('/login', methods=['POST'])
 def login():
     from models import User
@@ -57,16 +58,18 @@ def login():
 
     return jsonify({'id': user.id, 'email': user.email, 'password': user.password, 'token': token })
 
+# Rota de quantidade de crimes por ano (Sérgio)
 @app.route('/quantidade/crimes/<ano>', methods=['GET'])
-def qtd_crime_ano(ano, mes):
+def qtd_crime_ano(ano):
     token = request.headers.get('Authorization').split(' ')[1]
     if not(authorization(token)):
         return jsonify({'msg': 'Token inválido, faça login novamente'})
     
     print(ano)
 
-    return jsonify({'ano':ano, 'mes': mes})
+    return jsonify({'ano':ano})
 
+# Rota de quantidade de determinada ocorrencia por estado (Sérgio)
 @app.route('/quantidade/ocorrencias/<nome>/<sigla>', methods=['GET'])
 def qtd_ocorrencias_nome_sigla(nome, sigla):
     token = request.headers.get('Authorization').split(' ')[1]
@@ -77,6 +80,7 @@ def qtd_ocorrencias_nome_sigla(nome, sigla):
 
     return jsonify({'nome':nome, 'sigla': sigla})
 
+# Rota de quantidade de determinadas vititmas por estado (Raíssa)
 @app.route('/quantidade/vitimas/<nomedocrime>/<sigla>', methods=['GET'])
 def qtd_vitimas_nome_sigla(nomedocrime, sigla):
     token = request.headers.get('Authorization').split(' ')[1]
@@ -87,6 +91,7 @@ def qtd_vitimas_nome_sigla(nomedocrime, sigla):
 
     return jsonify({'nomedocrime':nomedocrime, 'sigla': sigla})
 
+# Média mensal de ocorrenias por estado dentro de um período (Raíssa)
 @app.route('/media/ocorrencias/<nomedocrime>/<sigla>/<inicio>/<fim>', methods=['GET'])
 def media_ocorrencias_nome_sigla_periodo(nomedocrime, sigla, inicio, fim):
     token = request.headers.get('Authorization').split(' ')[1]
@@ -97,6 +102,7 @@ def media_ocorrencias_nome_sigla_periodo(nomedocrime, sigla, inicio, fim):
 
     return jsonify({'nomedocrime':nomedocrime, 'sigla': sigla, 'inicio': inicio, 'fim': fim})
 
+# Media mensal de vítimas por estado dentro de um período (Cadu)
 @app.route('/media/vitimas/<nomedocrime>/<sigla>/<inicio>/<fim>', methods=['GET'])
 def media_vitimas_nome_sigla_periodo(nomedocrime, sigla, inicio, fim):
     token = request.headers.get('Authorization').split(' ')[1]
@@ -107,6 +113,7 @@ def media_vitimas_nome_sigla_periodo(nomedocrime, sigla, inicio, fim):
 
     return jsonify({'nomedocrime':nomedocrime, 'sigla': sigla, 'inicio': inicio, 'fim': fim})
 
+# Ranking estadual por crime (Geronimo)
 @app.route('/ranking/<quantidade>/estadual/<nomedocrime>')
 def ranking_estadual_por_crime(quantidade, nomedocrime):
     token = request.headers.get('Authorization').split(' ')[1]
@@ -117,6 +124,7 @@ def ranking_estadual_por_crime(quantidade, nomedocrime):
 
     return jsonify({'nomedocrime':nomedocrime, 'quantidade': quantidade})
 
+# Ranking criminal por estado por estado (Lucas)
 @app.route('/ranking/<quantidade>/criminal/<sigla>')
 def ranking_criminal_por_estado(quantidade, sigla):
     token = request.headers.get('Authorization').split(' ')[1]
